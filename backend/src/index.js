77affd0c9e7ddd5be361d38da53f3aa51c1b7367
savio <saviopf22@gmail.com>
2020-08-18 02:49:05 -0300

    testando verbos do http, parametros e retornos

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,36 @@ const app = express();
 
 app.use(express.json());
 
-// url test
-// http://localhost:3333/teste/savio?lastname=fontes
+// EXAMPLES
+
 app.get("/test/:name/:lastname?", (req, res) => {
   console.log(`${req.method}: localhost:${port}/test`);
   return res.json({
     msg: `hello world ${req.params.name} ${req.query.lastname}`,
+  });
+});
+
+app.post("/test", (req, res) => {
+  console.log(`${req.method}: localhost:${port}/test`);
+  return res.json({ result: { ...req.body, auth: `${req.headers.authtest}` } });
+});
+
+app.put("/test/:id", (req, res) => {
+  console.log(`${req.method}: localhost:${port}/test`);
+  return res.json({
+    result: {
+      ...req.body,
+      id: req.params.id,
+      auth: `${req.headers.authtest}`,
+    },
+  });
+});
+
+app.delete("/test/:id", (req, res) => {
+  console.log(`${req.method}: localhost:${port}/test`);
+  return res.json({
+    msg: `deleted: ${req.params.id}`,
+    auth: `${req.headers.authtest}`,
   });
 });
 
