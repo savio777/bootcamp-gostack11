20c00f5432d866d52a28f0018558789d6a807c0a
savio <saviopf22@gmail.com>
2020-08-18 21:39:42 -0300

    comentarios com informações sobre os verbos http e parametros

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,30 @@ const app = express();
 
 app.use(express.json());
 
-// EXAMPLES
+/*
+  VERBOS HTTP:
+  
+  GET~> Buscar informações da api;
+  POST~> Criar uma informação na api;
+  PUT/PATCH~> Alterar uma informação na api;
+  DELETE~> Deletar uma informação na api;
+*/
+
+/*
+  Uso Geral de Parâmetros:
+  
+  Query~> filtros e paginação, 
+  ex:localhost:3333/teste?teste=teste ;
+  
+  Route~> identificar recursos (atualizar/deletar), 
+  ex:localhost:3333/teste/teste
+  
+  Body~> informações para criar ou editar um recurso (JSON),
+  ex: {"teste":"teste"};
+
+  Header~> autenticar usuário ou passar alguma configuração para a api,
+  ex: "Auth": "87f633634cc4b02f628685651f0a29b7bfa22a0bd841f725c6772dd00a58d489";
+*/
 
 app.get("/test/:name/:lastname?", (req, res) => {
   console.log(`${req.method}: localhost:${port}/test`);
