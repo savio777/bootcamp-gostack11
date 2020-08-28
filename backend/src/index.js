c54c97fb44a9685ba91745e682a47ea50f3a080e
savio <saviopf22@gmail.com>
2020-08-28 02:11:45 -0300

    adição do cors para que possa ser requisitado pelo frontend em localhost

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { uuid, isUuid } = require("uuidv4");
 
 const port = 3333;
@@ -6,6 +7,7 @@ const port = 3333;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const projectsTest = [];
 
