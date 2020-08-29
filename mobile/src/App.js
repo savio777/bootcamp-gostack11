218dec8f6973fbb4d1cb88f8eb85928f6e59aa62
savio <saviopf22@gmail.com>
2020-08-29 00:37:18 -0300

    usando stylesheet, diferenças e coisas iguais no reactjs

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const App = () => (
-  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <Text>Hello react-native</Text>
+  <View style={styles.container}>
+    <Text style={styles.title}>Hello react-native</Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#7159',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
