d5ffc9013ed762220d5a4dd666e0ed6d04078d5a
savio <saviopf22@gmail.com>
2020-08-28 23:21:54 -0300

    init do app

---
+++ mobile/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Hello react-native</Text>
+  </View>
+);
+
+export default App;
