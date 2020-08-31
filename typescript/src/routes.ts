542756c3b0d208b2930e10d0077a1f9791fc05ef
savio <saviopf22@gmail.com>
2020-08-31 00:41:47 -0300

    criando tipos com interface

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express'
+import createUser from './services/CreateUser'
 
 export function hello(req: Request, res: Response) {
+//  const userTest = createUser({ name: 'savio', email: 'savio@gmail.com', password: '1234', adult: false, techs: ['js'] })
+  const user = createUser({ name: 'savio', email: 'savio@gmail.com', password: '1234', age: 12, adult: false, techs: [{ title: 'py', xp: 70 }] })
+
+  console.log(user.name, user.techs[0].title)
   return res.json({ msg: `${req.method} hello world` })
 }
