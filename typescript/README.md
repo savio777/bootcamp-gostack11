b3eb0e9ac2f303ec73166abcb8fc95f2a920b5c6
savio <saviopf22@gmail.com>
2020-09-01 23:56:04 -0300

    remoção dos projetos para iniciar nivel 02

--- typescript/README.md
+++
@@ -1,6 +0,0 @@
-# Commands for start
-
-1. npm run tsc
-2. npm start
-
-[cheatsheet/dicas](https://www.notion.so/Typescript-5712aeab312d44fcba0aa88895caad36)
--- typescript/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import {hello} from './routes'
-
-const app = express()
-
-app.get('/', hello)
-
-app.listen(3000, () => { console.log('server init') })
