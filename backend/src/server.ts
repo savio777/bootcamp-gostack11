1c95e229724d79bba15e1eee06bea4c499fadfb3
savio <saviopf22@gmail.com>
2020-09-08 00:56:52 -0300

    maneira de entregar arquivo estatico na api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './configs/upload';
+import ensureAuthenticated from './middlewares/ensureAuthenticated';
 
 import './database';
 
@@ -9,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/files', ensureAuthenticated, express.static(uploadConfig.directory));
 app.use(routes);
 
 app.get('/', (req, res) => {
