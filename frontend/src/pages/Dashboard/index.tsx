c325ac6a627c2719fdd35fadae5f08d56f065144
savio <saviopf22@gmail.com>
2020-09-13 11:48:37 -0300

    estilização dos cards dos repositorios completo

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
 import logo from "../../assets/logo.svg";
 
-import { Title, Form } from "./styles";
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => (
   <>
@@ -12,6 +13,51 @@ const Dashboard: React.FC = () => (
       <input placeholder="Digite o nome do repositório" />
       <button>Pesquisar</button>
     </Form>
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars3.githubusercontent.com/u/35678887?v=4"
+          alt="savio"
+        />
+        <div>
+          <strong>savio777/aircnc</strong>
+          <p>
+            aplicação criada a partir do curso grátis
+          </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars3.githubusercontent.com/u/35678887?v=4"
+          alt="savio"
+        />
+        <div>
+          <strong>savio777/aircnc</strong>
+          <p>
+            aplicação criada a partir do curso grátis
+          </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars3.githubusercontent.com/u/35678887?v=4"
+          alt="savio"
+        />
+        <div>
+          <strong>savio777/aircnc</strong>
+          <p>
+            aplicação criada a partir do curso grátis
+          </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
   </>
 );
 
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -12,7 +12,7 @@ export const Title = styled.h1`
 
 export const Form = styled.form`
   margin-top: 40px;
-  max-height: 700px;
+  max-width: 700px;
   display: flex;
 
   input {
@@ -40,6 +40,57 @@ export const Form = styled.form`
 
     &:hover {
       background: ${shade(0.2, "#04d361")};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    + a { /* ou & + a */
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
     }
   }
 `;
