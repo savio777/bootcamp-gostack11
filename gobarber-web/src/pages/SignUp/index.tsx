14cb67b3dfee4d48d4f0910fba9328d72eb90ddc
savio <saviopf22@gmail.com>
2021-03-17 13:17:42 -0300

    iniciando tela de cadastro

---
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {
+  FiArrowLeft, FiMail, FiLock, FiUser,
+} from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <h1>Faça seu cadastro</h1>
+        <Input icon={FiUser} name="name" type="text" placeholder="Email" />
+        <Input icon={FiMail} name="email" type="email" placeholder="Email" />
+        <Input icon={FiLock} name="pass" type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
+        <a href="forget">Esqueci minha senha</a>
+      </form>
+      <a href="#">
+        <FiArrowLeft />
+        Voltar para login
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignUp;
