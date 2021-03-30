ef01c34d4e558988fd439a539692383b372f2fa0
savio <saviopf22@gmail.com>
2021-03-30 15:30:36 -0300

    adição de icons e componentes do formulario

---
+++ gobarberApp/src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { RectButtonProperties } from 'react-native-gesture-handler';
+
+import { Container, ButtonText } from './styles';
+
+const Button: React.FC<RectButtonProperties> = ({ children, ...rest }) => (
+  <Container {...rest}>
+    <ButtonText>{children}</ButtonText>
+  </Container>
+);
+
+export default Button;
--- gobarberApp/src/screens/SignIn/index.tsx
+++ gobarberApp/src/screens/SignIn/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 import logo from '../../assets/logo.png';
@@ -11,6 +14,9 @@ const SignIn: React.FC = () => (
     <Image source={logo} width={100} height={50} />
 
     <Title>Faça seu login</Title>
+    <Input name="email" placeholder="Email" icon="mail" />
+    <Input name="password" placeholder="Senha" icon="lock" />
+    <Button onPress={() => {}}>Entrar</Button>
   </Container>
 );
 
